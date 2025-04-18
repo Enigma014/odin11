952aa58c896bbf6c1208b2010aeaae7e9ec356b9
Izma <101977312+Enigma014@users.noreply.github.com>
2025-04-19 00:09:48 +0530

    new

--- src/score.jsx
+++ src/score.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-function KeepScore({score}){
+function KeepScore({score,bestscore}){
 
-    return <h1>score:{score}</h1>;
+    return (
+        <>
+         <h1>score:{score}</h1>
+         <h1>bestscore:{bestscore}</h1>
+        </>
+   
+    );
     
 }
 export default KeepScore;
--- src/toggle.jsx
+++ src/toggle.jsx
@@ -4,19 +4,21 @@ import GifSearch from "./gif";
 function Toggle(){
     const[score,setScore]=useState(0);
     const[clickedGifs,setClickedGifs]=useState([]);
-
+    const[bestscore,setBestScore]=useState(0);
     const handleGifClick=(gifId)=>{
         if (!clickedGifs.includes(gifId)) {
             setClickedGifs([...clickedGifs,gifId]);
             setScore((prev)=>prev+1);
         }
         else{
+            setBestScore(score);
+            setClickedGifs([]);
             setScore(0);
         }
     }
     return(
         <div>
-            <KeepScore score={score}/>
+            <KeepScore score={score} bestscore={bestscore}/>
             <GifSearch onGifClick={handleGifClick}/>
         </div>
     )
